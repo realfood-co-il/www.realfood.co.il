50ed48ceaf5b4a2784ad9db9ae3af97655f83433
Evgeny Zislis <evgeny.zislis@gmail.com>
2020-01-31 20:57:07 +0200

    add editor config files

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es6": true,
+    "node": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:vue/essential"
+  ],
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  "parserOptions": {
+    "ecmaVersion": 2018
+  },
+  "plugins": [
+    "gridsome",
+    "vue"
+  ],
+  "rules": {
+    "gridsome/format-query-block": "error"
+  }
+};
